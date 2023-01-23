9f8415d0f5ef07638375d9a17a3c17beb3e06c36
eiki0920 <s2122039@stu.musashino-u.ac.jp>
2023-01-23 19:12:04 +0900

    create test screan(room)

--- app.py
+++ app.py
@@ -3,24 +3,52 @@ import random
 import requests
 import json
 
-st.title('APIテスト画面(ユーザー)')
+page = st.sidebar.selectbox('Choose your page', ['users', 'rooms'])
+if page == 'users':
+    st.title('APIテスト画面(ユーザー)')
 
-with st.form(key='user'):
-    user_id: int = random.randint(0, 10)
-    username: str = st.text_input('ユーザー名', max_chars=12)
-    data = {
-        'user_id': user_id,
-        'user_name': username
-    }
-    submit_button = st.form_submit_button(label='送信')
+    with st.form(key='user'):
+        user_id: int = random.randint(0, 10)
+        username: str = st.text_input('ユーザー名', max_chars=12)
+        data = {
+            'user_id': user_id,
+            'user_name': username
+        }
+        submit_button = st.form_submit_button(label='送信')
 
-if submit_button:
-    st.write('## 送信データ')
-    st.json(data)
-    st.write('## レスポンンス結果')
-    url = 'http://127.0.0.1:8000/users'
-    res = requests.post(
-        url,
-        data=json.dumps(data)
-    )
-    st.json(res.json())
+    if submit_button:
+        st.write('## 送信データ')
+        st.json(data)
+        st.write('## レスポンンス結果')
+        url = 'http://127.0.0.1:8000/users'
+        res = requests.post(
+            url,
+            data=json.dumps(data)
+        )
+        st.json(res.json())
+    
+elif page == 'rooms':
+    st.title('APIテスト画面(会議室)')
+    with st.form(key='room'):
+        room_id: int = random.randint(0, 10)
+        room_name: str = st.text_input('会議室名', max_chars=12)
+        capacity: int = st.number_input('定員', step=1)
+        data = {
+            'room_id': room_id,
+            'room_name': room_name,
+            'capacity': capacity
+        }
+        submit_button = st.form_submit_button(label='送信')
+
+    if submit_button:
+        st.write('## 送信データ')
+        st.json(data)
+        st.write('## レスポンンス結果')
+        url = 'http://127.0.0.1:8000/rooms'
+        res = requests.post(
+            url,
+            data=json.dumps(data)
+        )
+        st.json(res.json())
+
+    
