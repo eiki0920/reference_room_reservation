8e8631d730dea39b5c31611959bca4afa3cdc9a8
eiki0920 <s2122039@stu.musashino-u.ac.jp>
2023-01-25 00:58:38 +0900

    create cruds

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from .database import Base
 
-class Use(Base):
+class User(Base):
     __tablename__ = 'users'
     user_id = Column(Integer, primary_key=True, index=True)
     user_name = Column(String, unique=True, index=True)
