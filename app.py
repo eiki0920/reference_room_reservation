694c9c5dc8ecbec114364cf39ff122353b4a91a7
eiki0920 <s2122039@stu.musashino-u.ac.jp>
2023-01-26 19:06:02 +0900

    Processing when there is a duplication of approximate time

--- app.py
+++ app.py
@@ -157,8 +157,18 @@ elif page == 'bookings':
             ).isoformat()
             
         }
-        # 定員以下の予約人数の場合
-        if booked_num <= capacity:
+        # 定員より多い予約人数の場合
+        if booked_num > capacity:
+            st.error(f'{room_name}の定員は、{capacity}名です。{capacity}名以下の予約人数を受け付けています')
+        
+        # 開始時刻 >= 終了時刻
+        elif start_time >= end_time:
+            st.error('開始時刻が終了時刻を超えています')
+
+        elif start_time < datetime.time(hour=9, minute=0, second=0) or end_time > datetime.time(hour=20, minute=0, second=0):
+            st.error('利用時間は9:00~20:00になります。')
+
+        else:
             # 会議室予約
             url = 'http://127.0.0.1:8000/bookings'
             res = requests.post(
@@ -167,8 +177,8 @@ elif page == 'bookings':
             )
             if res.status_code == 200:
                 st.success('予約完了しました')
-            st.json(res.json())
-        else:
-            st.error(f'{room_name}の定員は、{capacity}名です。{capacity}名以下の予約人数を受け付けています')
+            elif res.status_code == 404 and res.json()['detail'] == 'Already booked':
+                st.error('指定の時間にはすでに予約が入っています')
+
 
     
